b18388284ad4baac4d3d8a531ba119f73b65b1d2
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-07-20 18:42:58 -0500

    Insere projeto bootstrap no repositório

---
+++ bootstrap-project/js/scripts.js
@@ -0,0 +1,102 @@
+const containerA = document.querySelector('#circleA');
+const containerB = document.querySelector('#circleB');
+const containerC = document.querySelector('#circleC');
+const containerD = document.querySelector('#circleD');
+
+  let circleA = new ProgressBar.Circle(containerA, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+  
+        var value = Math.round(circle.value() * 60);
+        circle.setText(value);
+  
+      }
+
+  });
+
+  let circleB = new ProgressBar.Circle(containerB, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 2000,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+  
+        var value = Math.round(circle.value() * 254);
+        circle.setText(value);
+  
+      }
+
+  });
+
+  let circleC = new ProgressBar.Circle(containerC, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1500,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+  
+        var value = Math.round(circle.value() * 190);
+        circle.setText(value);
+  
+      }
+
+  });
+
+  let circleD = new ProgressBar.Circle(containerD, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1600,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+  
+        var value = Math.round(circle.value() * 200);
+        circle.setText(value);
+  
+      }
+
+  });
+
+// Iniciando os loaders quando o usuário chegar no elemento
+let dataArea = document.querySelector('#data-area');
+let dataAreaOffset = dataArea.offsetTop;
+// "stop" serve para a animação não carregar mais que uma vez
+let stop = 0;
+
+function handleScroll() {
+  let scroll = window.scrollY || window.pageYOffset;
+
+  if (scroll > dataAreaOffset - 600 && stop === 0) {
+    circleA.animate(1.0);
+    circleB.animate(1.0);
+    circleC.animate(1.0);
+    circleD.animate(1.0);
+
+    stop = 1;
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+// Parallax
+setTimeout(function() {
+    $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png' })
+}, 250)
