a00971e2204e17ad1a8bc3581bed105d5fdd78f7
vrcvieira <victor.vieira@ifac.edu.br>
2023-07-25 20:38:26 -0500

    Update scripts.js

--- bootstrap-project/scripts.js
+++ bootstrap-project/scripts.js
@@ -118,3 +118,54 @@ containerD.animate(1.0);
 $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
 $('#apply-content').parallax({imageSrc: 'img/pattern.png'})
 
+// Scroll para as seções da página a partir do menu
+
+let navBtn = $('.nav-item');
+
+let bannerSection = $('#carousel-area');
+let aboutSection = $('#about-area');
+let servicesSection = $('#services-area');
+let teamSection = $('#team-area');
+let portfolioSection = $('#portfolio-area');
+let contactSection = $('#contact-area');
+
+let scrollTo = '';
+
+$(navBtn).click(function() {
+  
+  let btnId = $(this).attr('id');
+  
+  if(btnId == 'about-menu') {
+    scrollTo = aboutSection;
+  } else if(btnId == 'services-menu') {
+    scrollTo = servicesSection;
+  } else if(btnId == 'team-menu') {
+    scrollTo = teamSection;
+  } else if(btnId == 'portfolio-menu') {
+    scrollTo = portfolioSection;
+  } else if(btnId == 'contact-menu') {
+    scrollTo = contactSection;
+  } else {
+    scrollTo = bannerSection;
+  }
+  
+  $([document.documentElement, document.body]).animate({      scrollTop: $(scrollTo).offset().top - 70  
+}, 1500);});
+
+// Filtro para a seção de portfólio
+
+// pegue os estados dos botões (qual está selecionado)
+// pegue os cards
+
+// adicione um evento aos botões
+
+// funçao de filtro  
+  // se o filtro não está marcado para "todos"    
+    // para cada card      
+      // se o card não corresponde ao filtro        
+        // esconder o card       
+      // senão, ou seja, se o card corresponde ao filtro         
+        // mostrar o card   
+  // senão, ou seja, se o filtro está em todos    
+    // para cada card      
+      // mostre os elementos cards 
